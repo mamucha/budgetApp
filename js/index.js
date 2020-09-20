47e8b130897f69db73b5c85babc91e1d93496cce
Marcin <marcin9213@gmail.com>
2020-09-20 11:56:36 +0200

    add all switch input logic

--- js/index.js
+++ js/index.js
@@ -5,7 +5,41 @@ btn.addEventListener('click', function () {
   document.querySelector('.c-add').classList.remove('c-add--content');
 });
 
-document.querySelector('[data-income]').addEventListener('click', (e) => {
-  document.querySelector('.c-add').classList.toggle('c-add--content');
-  document.querySelector('.c-content__title').innerHTML = 'Add Expense';
-});
+class BudgetApp {
+  addIncomes = null;
+  addExpense = null;
+  addTransfer = null;
+  addBill = null;
+
+  UISelectors = {
+    addIncomes: '[data-income]',
+    addExpense: '[data-expense]',
+    addTransfer: '[data-transfer]',
+    addBill: '[data-id]',
+  };
+
+  initApp() {
+    this.addIncomes = document.querySelector(this.UISelectors.addIncomes);
+    this.addExpense = document.querySelector(this.UISelectors.addExpense);
+    this.addTransfer = document.querySelector(this.UISelectors.addTransfer);
+    this.addBill = document.querySelectorAll(this.UISelectors.addBill);
+
+    this.addListeners();
+  }
+
+  addListeners() {
+    this.addBill.forEach((item) =>
+      item.addEventListener('click', () => {
+        this.addItem(item);
+      })
+    );
+  }
+
+  addItem(item) {
+    document.querySelector('.c-add').classList.toggle('c-add--content');
+    document.querySelector('.c-content__title').innerHTML = `Add ${item.dataset.id}`;
+  }
+}
+
+const budget = new BudgetApp();
+budget.initApp();
